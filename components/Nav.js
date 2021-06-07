d984eb1d2c5c3291ce9f1bfa6808cab4d46753ab
Peter Lau <superpeterlau@outlook.com>
2021-06-08 03:13:59 +0800

    basic pwa for next.js

---
+++ components/Nav.js
@@ -0,0 +1,92 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+const links = [
+  { href: "https://cn.bing.com", label: "Search0" },
+  { href: "https://cn.bing.com", label: "Search1" },
+  { href: "https://cn.bing.com", label: "Search2" },
+  { href: "https://cn.bing.com", label: "Search3" },
+].map((link) => ({
+  ...link,
+  key: `nav-link-${link.href}-${link.label}`,
+}));
+const Nav = () => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <title>Tools</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          {links.map(({ key, href, label }) => (
+            <li key={key}>
+              <a href={href}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <style jsx>
+        {`
+          :global(body) {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+              Helvetica, sans-serif;
+          }
+          nav {
+            display: flex;
+            text-align: center;
+          }
+          ul {
+            display: flex;
+            justify-content: space-between;
+          }
+          nav > ul {
+            padding: 4px 16px;
+          }
+          li {
+            display: flex;
+            padding: 6px 8px;
+          }
+          a {
+            color: #067df7;
+            text-decoration: none;
+            font-size: 13px;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default Nav;

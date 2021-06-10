ae4dd6fa61c660f02a7f667a7b4ece7339dbf930
Peter Lau <superpeterlau@outlook.com>
2021-06-10 16:44:24 +0800

    add qrcode and calendar

---
+++ pages/tools/qrcode.js
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState } from "react";
+import * as QRCode from "qrcode";
+
+export default function Qrcode() {
+  const [content, setContent] = useState("https://cn.bing.com");
+  const qrRef = useRef();
+
+  const handleInput = (e) => {
+    let content = e.target.value;
+    setContent(content);
+  };
+
+  const generateQR = async (qr, text) => {
+    try {
+      console.log("generateQR:", qr);
+      await QRCode.toCanvas(qr, text);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(async () => {
+    console.log(content);
+    if (content !== "" && qrRef && qrRef.current)
+      await generateQR(qrRef.current, content);
+  }, [content]);
+
+  return (
+    <div>
+      <div>
+        <textarea
+          value={content}
+          onPaste={handleInput}
+          onChange={handleInput}
+          rows={3}
+          placeholder={"Input Source"}
+        />
+      </div>
+      <canvas ref={qrRef}></canvas>
+    </div>
+  );
+}

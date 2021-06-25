df86ca7ca18488a32fe7e6799acb153f35be1b65
Peter Lau <superpeterlau@outlook.com>
2021-06-25 17:04:40 +0800

    add api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,27 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// https://vercel.com/docs/runtimes#official-runtimes/node-js
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+const puppeteer = require("puppeteer");
+
+const fetchBook = async (bookId) => {
+  let url = `https://book.douban.com/subject/${bookId}/`;
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+  await page.goto(url);
+  let book = await page.evaluate(() => {
+    element = document.querySelector(".subjectwrap");
+    return element.innerHTML;
+  });
+  await browser.close();
+  return book;
+};
+
+export default async (req, res) => {
+  // ?bookId=1234567
+  const { bookId } = req.query;
+  content = await fetchBook(bookId);
+  res
+    .status(200)
+    .json({ name: "John Doe", body: req.body, query: req.query, content });
+};

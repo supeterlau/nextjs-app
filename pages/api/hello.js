0c4fb431d55cd23f88d193d3ae18ecd5f00df2ec
Peter Lau <superpeterlau@outlook.com>
2021-06-26 15:24:34 +0800

    update pptr

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,41 @@
 // https://vercel.com/docs/runtimes#official-runtimes/node-js
 
-const puppeteer = require("puppeteer");
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const exePath =
+  process.platform === "win32"
+    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+
+export const getOptions = async (isDev) => {
+  /**
+   * If used in a dev environment, i.e. locally, use one of the local
+   * executable path
+   */
+  if (isDev) {
+    return {
+      args: [],
+      executablePath: exePath,
+      headless: true,
+    };
+  }
+  /**
+   * Else, use the path of chrome-aws-lambda and its args
+   */
+  return {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  };
+};
 
 const fetchBook = async (bookId) => {
   let url = `https://book.douban.com/subject/${bookId}/`;
-  const browser = await puppeteer.launch({
-    headless: true,
-  });
+  const options = await getOptions(isDev);
+  const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
   await page.goto(url);
   let book = await page.evaluate(() => {

7d960f41a1f4327e58063622d138ba938b962ee8
Peter Lau <superpeterlau@outlook.com>
2023-07-03 21:52:03 +0800

    test two handlers

--- api/index.go
+++ api/index.go
@@ -5,6 +5,10 @@ import (
   "net/http"
 )
  
-func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Go API!</h1>")
+func Handler1(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Go API! #Handler 1#</h1>")
+}
+
+func Handler2(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Go API! #Handler 2#</h1>")
 }
